a63be96c84e746638fb899fac411a53987c4585a
Airu Song <airusong@Airus-MacBook-Pro-2.local>
2024-11-13 18:18:54 -0800

    add pos validator

---
+++ scripts/2.invalid-pos-topic-create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the Kafka installation path
+KAFKA_HOME="/Users/airusong/Desktop/learningcontent/kafka/transactional-producer/kafka_2.13-3.9.0"
+
+# Variables
+ZOOKEEPER="localhost:2181"
+REPLICATION_FACTOR=3
+PARTITIONS=3
+TOPIC_NAME="invalid-pos"
+
+# Create Kafka topic
+$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor "$REPLICATION_FACTOR" --partitions "$PARTITIONS" --topic "$TOPIC_NAME"
---
+++ scripts/4.invalid-pos-consumer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Variables
+BOOTSTRAP_SERVER="localhost:9092"
+TOPIC_NAME="invalid-pos"
+KAFKA_HOME="/Users/airusong/Desktop/learningcontent/kafka/transactional-producer/kafka_2.13-3.9.0"
+# Start Kafka console consumer
+$KAFKA_HOME/bin/kafka-console-consumer.sh --bootstrap-server "$BOOTSTRAP_SERVER" --topic
